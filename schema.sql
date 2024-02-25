23516db55981c9aea8165c5a427b2a580df2afa0
matsu <tmtmt2209@gmail.com>
2024-02-25 09:12:06 +0000

    chore: update schema

--- schema.sql
+++ schema.sql
@@ -1,56 +1,56 @@
-create table public.users (
+create table users (
     id uuid primary key
 );
 
-create table public.user_profile (
+create table user_profile (
     user_id uuid primary key,
     email text not null,
     name text,
     created_at timestamp not null default current_timestamp,
 
-    constraint fk_user_profile_user foreign key (user_id) references public.users (id)
+    constraint fk_user_profile_user foreign key (user_id) references users (id)
 );
 
-create index idx_user_profile_email on public.user_profile using btree (email);
+create index idx_user_profile_email on user_profile using btree (email);
 
-create table public.user_delete (
+create table user_delete (
     user_id uuid primary key,
     created_at timestamp not null default current_timestamp,
 
-    constraint fk_user_delete_user foreign key (user_id) references public.users (id)
+    constraint fk_user_delete_user foreign key (user_id) references users (id)
 );
 
-create table public.organizations (
+create table organizations (
     id uuid primary key
 );
 
-create table public.organization_profile (
+create table organization_profile (
     organization_id uuid primary key,
     name text not null,
     created_at timestamp not null default current_timestamp,
 
     constraint fk_organization_profile_organization foreign key (
         organization_id
-    ) references public.organizations (id)
+    ) references organizations (id)
 );
 
-create table public.organization_delete (
+create table organization_delete (
     organization_id uuid primary key,
     created_at timestamp not null default current_timestamp,
 
     constraint fk_organization_delete_organization foreign key (
         organization_id
-    ) references public.organizations (id)
+    ) references organizations (id)
 );
 
-create table public.belong (
+create table belong (
     user_id uuid not null,
     organization_id uuid not null,
     created_at timestamp not null default current_timestamp,
 
     primary key (user_id, organization_id),
-    constraint fk_belong_user foreign key (user_id) references public.users (id),
+    constraint fk_belong_user foreign key (user_id) references users (id),
     constraint fk_belong_organization foreign key (
         organization_id
-    ) references public.organizations (id)
+    ) references organizations (id)
 );
