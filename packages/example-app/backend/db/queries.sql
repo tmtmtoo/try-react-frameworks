0643d2a8fd38dc99dc5ab74246dd18d685b9a48b
matsu <tmtmt2209@gmail.com>
2024-03-17 09:14:41 +0000

    test(db): user

--- packages/example-app/backend/db/queries.sql
+++ packages/example-app/backend/db/queries.sql
@@ -1,10 +1,17 @@
--- name: GetUser :one
-select
-    user_id as id,
-    email,
-    name,
-    created_at
-from user_profile
-where id = $1
+-- name: InsertUser :exec
+insert into users (id) values ($1);
+
+-- name: InsertUserProile :exec
+insert into user_profile (id, user_id, email, name) values ($1, $2, $3, $4);
+
+-- name: SelectLatestUserProfileByEmail :one
+select * from user_profile
+where email = $1
 order by created_at desc
 limit 1;
+
+-- name: DeleteUser :exec
+insert into user_delete (id, user_id) values ($1, $2);
+
+-- name: DeleteUserProfile :exec
+delete from user_profile where id = $1;
