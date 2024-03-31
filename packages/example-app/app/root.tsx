c0554b7280d77844cb703090fec7710c562b0642
matsu <tmtmt2209@gmail.com>
2024-03-31 10:28:39 +0000

    chore: login page

--- packages/example-app/app/root.tsx
+++ packages/example-app/app/root.tsx
@@ -29,7 +29,7 @@ export default function App() {
                 <Outlet />
                 <ScrollRestoration />
                 <Scripts />
-                <LiveReload />
+                {/* <LiveReload /> */}
             </body>
         </html>
     );
